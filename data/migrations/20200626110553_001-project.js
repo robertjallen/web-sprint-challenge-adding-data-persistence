b17009a361532649a74695432702b4b8403d3991
robertjallen <bobbidigi34@gmail.com>
2020-06-26 12:19:24 -0400

    add database schema design and migrations

--- data/migrations/20200626110553_001-project.js
+++ data/migrations/20200626110553_001-project.js
@@ -1,8 +1,56 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable('project', tbl => {
+    // creates a primary key called id
+    tbl.increments("id");
+    // creates a text field called name which is both required and unique
+    tbl.text('name').unique().notNull();
+    tbl.text('description').notNull();
+    tbl.boolean('completed').defaultTo(false)
+  });
+
+  await knex.schema.createTable('task', tbl => {
+    // creates a text field called name which is both required and unique
+    tbl.text('notes').unique().notNull();
+    tbl.text('description').notNull();
+    tbl.boolean('completed').defaultTo(false)
+
+    //foreign key
+    tbl
+      .integer("project_id")
+      .references("id")
+      .inTable("project")
+      .onDelete("SET NULL")
+  });
+
+  await knex.schema.createTable('resource', tbl => {
+    // creates a primary key called id
+    tbl.increments("id");
+    // creates a text field called name which is both required and unique
+    tbl.text('name').unique().notNull();
+    tbl.text('description').notNull();
+  });
+
+  await knex.schema.createTable('projects_resources', tbl => {
+    //foreign key
+    tbl
+      .integer("project_id")
+      .references("id")
+      .inTable("project")
+      .onDelete("SET NULL")
+
+    //foreign key
+    tbl
+      .integer("resource_id")
+      .references("id")
+      .inTable("resource")
+      .onDelete("SET NULL")  
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('projects_resources');
+  await knex.schema.dropTableIfExists('resource');
+  await knex.schema.dropTableIfExists('task');
+  await knex.schema.dropTableIfExists('project');
 };
